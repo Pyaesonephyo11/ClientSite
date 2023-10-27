1ac16b914ae31b2072674dcac8a5b772fbdaa2a2
Pyaesonephyo11 <phoehtwar136@gmial.com>
2023-10-27 16:31:45 +0630

    Initial commit

---
+++ app/src/main/java/com/trustlinkmm/clientsite/MainActivity.java
@@ -0,0 +1,156 @@
+package com.trustlinkmm.clientsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
+@SuppressLint("SetTextI18n")
+public class MainActivity extends AppCompatActivity {
+    Thread Thread1 = null;
+    EditText etIP, etPort;
+    TextView tvMessages;
+    EditText etMessage;
+    Button btnSend;
+    String SERVER_IP;
+    int SERVER_PORT;
+
+
+
+    //psp
+    ServerSocket serverSocket;
+
+    Socket socket;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etIP = findViewById(R.id.etIP);
+        etPort = findViewById(R.id.etPort);
+        tvMessages = findViewById(R.id.tvMessages);
+        etMessage = findViewById(R.id.etMessage);
+        btnSend = findViewById(R.id.btnSend);
+        Button btnConnect = findViewById(R.id.btnConnect);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMessages.setText("");
+                SERVER_IP = etIP.getText().toString().trim();
+                SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
+                Thread1 = new Thread(new Thread1());
+                Thread1.start();
+            }
+        });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = etMessage.getText().toString().trim();
+                if (!message.isEmpty()) {
+                    new Thread(new Thread3(message)).start();
+
+                    Thread1 = new Thread(new Thread1());
+                    Thread1.start();
+                }
+            }
+        });
+
+    }
+    private DataOutputStream output;
+    private BufferedReader input;
+    class Thread1 implements Runnable {
+        public void run() {
+
+            try {
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                output = new DataOutputStream(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvMessages.setText("Connected");
+                    }
+                });
+                String message = etMessage.getText().toString().trim();
+                if (!message.isEmpty()) {
+                    new Thread(new Thread3(message)).start();
+                }
+                new Thread(new Thread2()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                               //tvMessages.append("server: " + message + "");
+                                Toast.makeText(MainActivity.this,"server:" + message,Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class Thread3 implements Runnable {
+        private String message;
+        Thread3(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            try {
+                output.writeBytes(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                output.flush();
+                output.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //tvMessages.append("client: " + message + "");
+                            etMessage.setText("");
+                }
+            });
+        }
+    }
+
+}
